a95e6e47c3a75e10ad7dc25c550e3197a6e6468f
LsMin124 <xleex0124@gmail.com>
2025-10-19 12:59:51 +0900

    feat: 결과 출력 및 오류 처리기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,13 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+
+        try {
+            StringCalculator calculator = new StringCalculator();
+            int result = calculator.add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.err.println("오류 : " + e.getMessage());
+        }
     }
 }
