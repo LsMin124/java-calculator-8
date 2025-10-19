7437b3f4afd1d8dce07d277d7c7b78a294cb34ae
LsMin124 <xleex0124@gmail.com>
2025-10-19 14:11:10 +0900

    feat: 파싱한 입력값에 대한 정수 전환 및 예외처리 기능 구현

--- src/main/java/converter/NumberConverter.java
+++ src/main/java/converter/NumberConverter.java
@@ -1,4 +1,32 @@
 package converter;
 
+import java.util.Arrays;
+
 public class NumberConverter {
+
+    public int[] convert(String[] stringNumbers) {
+        return Arrays.stream(stringNumbers)
+                .mapToInt(this::parseAndValidate)
+                .toArray();
+    }
+
+    private int parseAndValidate(String stringNumber) {
+        int number = parseToInt(stringNumber);
+        validateNegative(number);
+        return number;
+    }
+
+    private int parseToInt(String stringNumber) {
+        try {
+            return Integer.parseInt(stringNumber.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력값에 숫자가 아닌 값이 존재합니다: ", e);
+        }
+    }
+
+    private void validateNegative(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("입력값에 음수가 포함되어 있습니다.");
+        }
+    }
 }
